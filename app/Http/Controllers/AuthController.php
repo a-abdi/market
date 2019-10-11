60a9d4c49319ffcbd931841edfe8492d626c1fce
ali abdi <ali.abdi.1991@gmail.com>
2019-10-11 21:45:36 +0330

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login_index() {
+        return view('auth.login-index');
+    }
+}
