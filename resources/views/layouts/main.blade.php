a33df0dd22ab38bcab98e05f92ed984a88cafd67
ali abdi <ali.abdi.1991@gmail.com>
2019-10-25 08:39:48 +0330

    create head.blade.php and nav.blade .php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{asset('js/app.js')}}" defer></script>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('css/auth/login.css')}}">
-    <title>ورود</title>
-</head>
-<body class="bg-b">
-    @yield('content')
-</body>
+    <head>
+        @include('layouts.head')
+        <title>Document</title>
+    </head>
+    <body class="bg-b">
+        <div id="app">
+            @include('layouts.nav')
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
 </html>
+
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="http://192.168.43.83:81">
+                Home
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://192.168.43.83:81/login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="http://192.168.43.83:81/register">Register</a>
+                        </li>
+                </ul>
+            </div>
+        </div>
+</nav>
